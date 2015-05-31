9bdc3ce0404aac43d0ba7a3db4c4913c9386204d
ryandong82 <31873601@qq.com>
2015-05-31 20:10:55 +0800

    更新数据库

--- sql/teacher_group.sql
+++ sql/teacher_group.sql
@@ -1,5 +1,5 @@
 ﻿# Host: localhost  (Version: 5.5.40)
-# Date: 2015-05-31 02:17:24
+# Date: 2015-05-31 20:10:01
 # Generator: MySQL-Front 5.3  (Build 4.120)
 
 /*!40101 SET NAMES utf8 */;
@@ -8,15 +8,18 @@
 # Structure for table "teacher_group"
 #
 
+DROP TABLE IF EXISTS `teacher_group`;
 CREATE TABLE `teacher_group` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `group_name` varchar(255) NOT NULL DEFAULT '',
   `group_orderno` float DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 #
 # Data for table "teacher_group"
 #
 
-INSERT INTO `teacher_group` VALUES (1,'私董会',NULL),(2,'投资家',NULL),(3,'聚娴女性',NULL),(4,'博雅国学智慧',NULL),(5,'博雅后EMBA高端项目',NULL);
+/*!40000 ALTER TABLE `teacher_group` DISABLE KEYS */;
+INSERT INTO `teacher_group` VALUES (1,'私董会',NULL),(2,'投资家',NULL),(3,'聚娴女性',NULL),(4,'国学智慧',NULL),(5,'后EMBA高端项目',NULL);
+/*!40000 ALTER TABLE `teacher_group` ENABLE KEYS */;
---
+++ sql/uploaded_res.sql
@@ -0,0 +1,18 @@
+﻿# Host: localhost  (Version: 5.5.40)
+# Date: 2015-05-31 20:09:37
+# Generator: MySQL-Front 5.3  (Build 4.120)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "uploaded_res"
+#
+
+DROP TABLE IF EXISTS `uploaded_res`;
+CREATE TABLE `uploaded_res` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `filename` varchar(255) NOT NULL DEFAULT '',
+  `create_time` datetime DEFAULT NULL,
+  `mime_type` varchar(90) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
