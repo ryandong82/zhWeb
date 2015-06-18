d192b5690b66738e1768d3c927e65d9269c301f0
ryandong82 <31873601@qq.com>
2015-06-18 10:58:56 +0800

    上传文章的保存添加事务处理 修复文件名错乱的问题

--- admin/new_article.php
+++ admin/new_article.php
@@ -8,35 +8,39 @@ $category = $_POST["article_category"];
 require_once 'pub_funcs.php';
 $is_matched = preg_match('/<img\ssrc="data:(?P<contenttype>.*?);base64,(?P<content>.*?)">/', $content, $matches);
 require_once("../dbconn.php");
-
+try {
+    $pdo->beginTransaction();
 //$is_matched = preg_match('/data:(?P<contenttype>.*?);base64/', $aa, $matches);
-if ($is_matched)
-{
-    $mime_type = $matches['contenttype'];
-    $img = base64_decode($matches['content']);
-    $photo_file = gen_file_name();
-    $new_file = $_SERVER['DOCUMENT_ROOT'] . "/statics/images/upload/$photo_file";
-    if (file_put_contents($new_file, $img))
-    {
-        $img_type = exif_imagetype($new_file);
-        $ext = image_type_to_extension($img_type, TRUE);
-        rename($new_file, $new_file . $ext);
-        $new_file = $new_file . $ext;
-        $content_nopic = preg_replace('/<img\ssrc="data:.*?;base64,.*?">/', "<img src=\"statics/images/upload/$photo_file" . "$ext\"", $content);
-        $sql = "insert into uploaded_res(filename, create_time, mime_type)
+    if ($is_matched) {
+        $mime_type = $matches['contenttype'];
+        $img = base64_decode($matches['content']);
+        $photo_file = gen_file_name();
+        $new_file = $_SERVER['DOCUMENT_ROOT'] . "/statics/images/upload/$photo_file";
+        if (file_put_contents($new_file, $img)) {
+            $img_type = exif_imagetype($new_file);
+            $ext = image_type_to_extension($img_type, TRUE);
+            rename($new_file, $new_file . $ext);
+            $new_file = $new_file . $ext;
+            $photo_file = $photo_file . $ext;
+            $content_nopic = preg_replace('/<img\ssrc="data:.*?;base64,.*?">/', "<img src=\"statics/images/upload/$photo_file" . "\"", $content);
+            $sql = "insert into uploaded_res(filename, create_time, mime_type)
         VALUES('$photo_file', now(), '$mime_type') ";
-        $pdo->exec($sql);
+            $pdo->exec($sql);
+        }
     }
-}
 
 //preg_replace('<img src="data:image/jpeg;base64,', $replacement, $subject)
 
 //$result = $pdo->query("select * from articles a join article_category b on a.category = b.Id");
-$sql = "insert into articles(title, content, create_time, category, keywords)
+    $sql = "insert into articles(title, content, create_time, category, keywords)
         VALUES('$title', '$content_nopic', now(), $category, null) ";
-if ($pdo->exec($sql)) {
-    echo json_encode(array("success" => "1", "new_id" => $pdo->lastInsertId()));
-} else {
-    echo json_encode(array("success" => "0", "error" => $pdo->errorInfo()));
+    if ($pdo->exec($sql)) {
+        echo json_encode(array("success" => "1", "new_id" => $pdo->lastInsertId()));
+    } else {
+        echo json_encode(array("success" => "0", "error" => $pdo->errorInfo()));
+    }
+    $pdo->commit();
+} catch (Exception $e) {
+    $pdo->rollBack();
+    http_response_code(500);
 }
-?>
